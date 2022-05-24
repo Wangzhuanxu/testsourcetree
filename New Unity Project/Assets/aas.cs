0dab1fd48ee945bb92d72f955a4ff202c1488f40
wangzhuanxu <1033428951@qq.com>
2022-05-24 23:41:06 +0800

    test merge

--- New Unity Project/Assets/aas.cs
+++ New Unity Project/Assets/aas.cs
@@ -7,7 +7,7 @@ public class aas : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //sdfsfsdfssssssssss
     }
 
     // Update is called once per frame
